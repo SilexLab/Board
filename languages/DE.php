263a3a26198b53009ef7604aa947b42567b6b412
NoxNebula <patrick.kl@live.de>
2011-04-03 15:11:28 +0200

    Added Language files

---
+++ languages/DE.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author		SilexBoard Team
+ *					Nox Nebula
+ * @copyright	2011 SilexBoard
+ */
+
+/* Langfile:	German */
+
+$this->Items['com.sbb.header.welcome']		= 'Willkommen';
+$this->Items['com.sbb.header.welcome_text']	= 'Herzlich willkommen auf {$Site} - {$Slogan}';
+?>
---
+++ languages/EN.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author		SilexBoard Team
+ *					Nox Nebula
+ * @copyright	2011 SilexBoard
+ */
+
+/* Langfile:	English */
+
+$this->Items['com.sbb.header.welcome']		= 'Welcome';
+$this->Items['com.sbb.header.welcome_text']	= 'Welcome on {$Site} - {$Slogan}';
+?>
