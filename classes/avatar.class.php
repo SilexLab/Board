c7c79cd07f3250bdaa4097e8ba2c0ba0f21cf8a3
Cadillaxx <erpelding_daniel@hotmail.com>
2011-04-06 15:51:22 +0200

    user.php and avatar class added

---
+++ classes/avatar.class.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @author		SilexBoard Team
+ *					Cadillaxx
+ * @copyright	2011 SilexBoard
+ */
+
+class Avatar {
+    protected $Email;
+
+    public function  __construct($Email, $Size) {
+        $this->Email = $Email;
+        $this->Size = $Size;
+        $this->URL = "http://www.gravatar.com/avatar/".md5(strtolower(trim($this->Email)))."?d=mm&s=".$this->Size;
+    }
+
+    public function GetURL() {
+        return $this->URL;
+    }
+
+    public function  __toString() {
+        return '<img src="'.$this->GetURL().'" style="width: '.$this->Size.'px; height: '.$this->Size.'px" alt="" />';
+    }
+}
+?>
