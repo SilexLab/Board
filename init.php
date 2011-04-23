4e2b465e7c5167896af2747fd032eacab79f8f74
Yannick Stephan <Yannick_bmx12@gmx.de>
2011-04-23 22:31:49 +0200

    after 10 minutes you will automatically logged out

--- init.php
+++ init.php
@@ -21,4 +21,24 @@ mysql::Connect($CFG_Host, $CFG_User, $CFG_Password, $CFG_Database);
 
 // default timezone (for date functions)
 date_default_timezone_set('Europe/Berlin');
+
+// after 10 minutes you will automatically logged out
+$login = new login;
+
+if($login->logged_in()) {
+	mysql::Select('sessions', 'Time', 'Salt=\''.session_id().'\'');
+	
+	while($row = mysql::FetchArray()) {
+		$lastTime = $row['Time'];
+	}	
+	
+	$timeFuture = time() + 10 * 6;
+	
+	if($timeFuture - $lastTime > 600) {
+		$login->DoLogout();
+	} else {
+		$update = array("Time" => time());
+		mysql::Update('sessions', $update, 'Salt=\''.session_id().'\'');
+	}
+}
 ?>
