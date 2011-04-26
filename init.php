c84acb94af9eb1bf19b3959d6b2c2a8e753a0245
Gillo Braun <braun.gillo@hotmail.com>
2011-04-26 14:30:29 +0200

    Changed error reporting. no E_NOTICE anymore

--- init.php
+++ init.php
@@ -6,7 +6,7 @@
  */
 // Error reporting
 ini_set('display_errors', 1);
-error_reporting(E_ALL | E_STRICT);
+error_reporting(E_ALL ^ E_NOTICE | E_STRICT);
 
 // Include required files
 require_once('config.inc.php');
