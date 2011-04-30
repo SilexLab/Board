b4d1974a8d07d744ebab90cc4e559edb8f97739d
NoxNebula <patrick.kl@live.de>
2011-04-30 20:57:31 +0200

    Re-managed SQL files

---
+++ Draft/sql/30.04.2011-Updated_Forums.sql
@@ -0,0 +1,2 @@
+INSERT INTO `forums` (`ID`, `Type`, `Parent`, `Title`, `Description`, `Position`, `Permission`, `Status`) VALUES
+(NULL, '0', '1', 'Kategorie in einer Kategorie ohne Beschreibung', '', '1', '', '0');
