0604ae182f950724ce27ce258116e9a7afdd05f5
NoxNebula <patrick.kl@live.de>
2011-06-08 21:22:42 +0200

    Added $language->GetLanguages(); to get the name of all available Languages - closes #44

--- classes/language.class.php
+++ classes/language.class.php
@@ -4,15 +4,18 @@
  * @copyright	© 2011 Silex Bulletin Board - Team
  * @license		GNU GENERAL PUBLIC LICENSE v3
  * @package		SilexBoard.DEV
- * @version		Revision: 1
+ * @version		Revision: 2
  */
 
 class language {
+	private $Languages = array();
+	
 	public $Items = array();
 	public $Default = 'DE';
+	
 	function __construct() {
 		// TODO: Auslesen der Sprache aus der MySQL-Datenbank und includen der selbigen
-		include_once(PATH_LANGUAGE.$this->Default.'.php');
+		include(PATH_LANGUAGE.$this->Default.'.php');
 	}
 	
 	public function Get($Key) {
@@ -23,6 +26,28 @@ class language {
 	public function Assign($TemplateObject) {
 		if($TemplateObject->IsTemplateObject)
 			$TemplateObject->AssignLanguage($this->Items);
+	}
+	
+	/* This function slow down the Page, use it carefully! */
+	public function GetLanguages() {
+		foreach(scandir(PATH_LANGUAGE) as $File) {
+			if(is_file(PATH_LANGUAGE.$File)) {
+				$Name = str_replace('.php', '', $File);
+				$GL = new GetLang($File);
+				$this->Languages[$Name] = $GL->GetName();
+			}
+		}
+		return $this->Languages;
+	}
+}
+
+class GetLang {
+	public $Items = array();
+	function __construct($File) {
+		include(PATH_LANGUAGE.$File);
+	}
+	public function GetName() {
+		return $this->Items['com.sbb.language'];
 	}
 }
 ?>
