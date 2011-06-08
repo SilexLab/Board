0bf80335e4c57729fd389f90daac1b82e83e1456
Gillo Braun <braun.gillo@hotmail.com>
2011-06-08 22:24:06 +0200

    Added new page

---
+++ pages/ChangeLanguage.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @author 		Angus
+ * @copyright	© 2011 Silex Bulletin Board - Team
+ * @license		GNU GENERAL PUBLIC LICENSE v3
+ * @package		SilexBoard.DEV
+ * @version		Revision: 1
+ */
+
+// Schutz vor Direktaufruf der Datei
+if(!defined('SILEX_VERSION'))
+	header('location: ../');
+if($_POST['language'])
+{
+	$lang = new GetLang($_POST['language'].'.php');
+	setcookie('sbb_DefLang',$_POST['language']);
+	$message = 'Ihre Sprache wurde in '.$lang->GetName().' geändert.';
+}
+self::$TPL->Assign('Content', $message);
+?>
