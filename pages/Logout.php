178ef77c6f30657b0c663d0c822ed96678d86459
Yannick Stephan <Yannick_bmx12@gmx.de>
2011-06-10 22:33:09 +0200

    Rename something, small CleanUp (Login)

--- pages/Logout.php
+++ pages/Logout.php
@@ -4,17 +4,17 @@
  * @copyright	© 2011 Silex Bulletin Board - Team
  * @license		GNU GENERAL PUBLIC LICENSE v3
  * @package		SilexBoard.DEV
- * @version		Revision: 2
+ * @version		Revision: 3
  */
 
 if(isset($_COOKIE['sbb_loginHash']) || session::Read('userid')) {
 	login::DoLogout();
-	$content = '{lang=com.sbb.logout.logged_out} <br>
+	$Content = '{lang=com.sbb.logout.logged_out} <br>
 				<a href="./">{lang=com.sbb.logout.main_menu}</a>';
 } else {
-	$content = '{lang=com.sbb.logout.never_logged_in} <br>
+	$Content = '{lang=com.sbb.logout.never_logged_in} <br>
 				<a href="./?page=Login">{lang=com.sbb.login.login}</a>';
 }
 
-self::$TPL->Assign('Content', $content);
+self::$TPL->Assign('Content', $Content);
 ?>
