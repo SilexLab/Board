b026471372db338b0f91a7af00250ed31edd1a25
NoxNebula <patrick.kl@live.de>
2011-07-04 07:09:58 +0200

    permission construct

---
+++ classes/permission.class.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @author 		Nox Nebula
+ * @copyright	© 2011 Silex Bulletin Board - Team
+ * @license		GNU GENERAL PUBLIC LICENSE v3
+ * @package		SilexBoard.DEV
+ * @version		Revision: 1
+ */
+
+class permission {
+	/**
+	 * Abgefragtes Zugriffsrecht wird über den $Permissions Parameter
+	 * übergeben. Zurückgegeben wird true, wenn der Zugriff erlaubt ist
+	 * und false, falls der Zugriff nicht erlaubt ist.
+	 * Als Obtionaler Parameter kann die ID von zum Beispiel einem Forum
+	 * übergeben werden.
+	 *
+	 * Beispiel: permission::Check('see.forum', 3); oder um auf alle Foren
+	 * bezug zu nehmen: permission::Check('see.forums');
+	 */
+	public static function Check($Permission, $ID = -1) {
+		return true;
+	}
+}
+?>
