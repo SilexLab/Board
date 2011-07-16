04bb48f664c459098477a5eed8019a1390c186c0
NoxNebula <patrick.kl@live.de>
2011-07-16 20:25:53 +0200

    removed old autoload, changed php header

--- lib/Includes/functions.inc.php
+++ lib/Includes/functions.inc.php
@@ -2,12 +2,10 @@
 /**
  * @author 		Nox Nebula
  * @copyright	© 2011 Silex Bulletin Board - Team
- * @license		GNU GENERAL PUBLIC LICENSE v3
- * @package		SilexBoard.DEV
- * @version		Revision: 1
+ * @license		GNU GENERAL PUBLIC LICENSE - Version3
+ * @package		SilexBoard
+ * @version		Revision: 2
  */
 
-function __autoload($className) {
-	include_once(PATH_CLASS.$className.'.class.php');
-}
+
 ?>
