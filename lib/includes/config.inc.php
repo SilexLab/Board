b69c9f135ee5585a9ea93ca2b970f1850e539893
NoxNebula <patrick.kl@live.de>
2011-07-18 17:49:48 +0200

    reset config.inc.php

--- lib/includes/config.inc.php
+++ lib/includes/config.inc.php
@@ -4,8 +4,8 @@ define('CFG_DB_PREFIX',		'');
 define('CFG_DB_TYPE',		'MySQL');
 define('CFG_DB_HOST',		'localhost');
 define('CFG_DB_USER',		'root');
-define('CFG_DB_PASSWORD',	'scarymovie4');
-define('CFG_DB_DATABASE',	'silex');
+define('CFG_DB_PASSWORD',	'');
+define('CFG_DB_DATABASE',	'silexboard');
 
 // Config aus Datenbank auslesen und Konstanten definieren (Config klasse)
 
