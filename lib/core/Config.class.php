179cf96a151d559c8b41ba0af4244fc86efed3b1
NoxNebula <patrick.kl@live.de>
2011-07-18 18:45:33 +0200

    compare non case sensitive

--- lib/core/Config.class.php
+++ lib/core/Config.class.php
@@ -13,7 +13,7 @@ class Config {
 		$Objects = MySQL::GetObjects();
 		foreach($Objects as $Obj) {
 			// When a Config has a "CFG_"-prefix, Create a constant
-			if(substr($Obj->ConfigName, 0, 4) == 'CFG_')
+			if(strtoupper(substr($Obj->ConfigName, 0, 4)) == 'CFG_')
 				SBB::CreateConstant(substr($Obj->ConfigName, 4), $Obj->ConfigValue);
 			else
 				Template::Assign(array($Obj->ConfigName => $Obj->ConfigValue));
