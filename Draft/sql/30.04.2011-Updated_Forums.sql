29d86ce3a79a53b96e83c018bff3deaa154f7d93
NoxNebula <patrick.kl@live.de>
2011-08-10 17:59:12 +0200

    New Database

--- Draft/sql/30.04.2011-Updated_Forums.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `forums` (`ID`, `Type`, `Parent`, `Title`, `Description`, `Position`, `Permission`, `Status`) VALUES
-(NULL, '0', '1', 'Kategorie in einer Kategorie ohne Beschreibung', '', '1', '', '0');
