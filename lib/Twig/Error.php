41386eb906bb8b6594906a60d8251afba80fb1b3
NoxNebula <patrick.kl@live.de>
2011-08-31 05:15:03 +0200

    Twig updated

--- lib/Twig/Error.php
+++ lib/Twig/Error.php
@@ -175,7 +175,7 @@ class Twig_Error extends Exception
             $tokens = token_get_all(file_get_contents($r->getFilename()));
             $templateline = -1;
             $template = null;
-            while ($token = array_shift($tokens)) {
+            foreach ($tokens as $token) {
                 if (isset($token[2]) && $token[2] >= $line) {
                     return array($templateline, $template);
                 }
