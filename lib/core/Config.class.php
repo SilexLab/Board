7cf0780a390dbde7df4916ffafd7f6268f5a6a09
Nox Nebula <patrick.kl@live.de>
2012-03-01 00:35:44 +0100

    improved Config class and prepare it for ACP access

--- lib/core/Config.class.php
+++ lib/core/Config.class.php
@@ -13,10 +13,13 @@ class Config {
 		if(!empty(self::$Config))
 			return false;
 		
-		$O = SBB::DB()->Table('config')->Select(array('ConfigNode', 'ConfigValue', 'TemplateVariable'))->Execute()->FetchObjects();
+		$O = SBB::DB()->Table('config')->Select(array('ConfigNode', 'ConfigValue', 'ValueType', 'TemplateVariable'))->Execute()->FetchObjects();
 		if(isset($O)) {
 			foreach($O as $C) {
-				self::$Config[$C->ConfigNode] = $C->ConfigValue;
+				$Value = ''; $Type = array();
+				$this->FormatValue($C->ConfigValue, $C->ValueType, $C->ConfigNode, $Value, $Type);
+				// TODO: for ACP include type
+				self::$Config[$C->ConfigNode] = $Value;
 				if($C->TemplateVariable)
 					self::$tplVariables[$C->TemplateVariable] = $C->ConfigValue;
 			}
@@ -31,6 +34,71 @@ class Config {
 	 */
 	public function Get($Node) {
 		return isset(self::$Config[$Node]) ? self::$Config[$Node] : null;
+	}
+
+	/* TODO: Beatify and improve the code starting here, it works but it's ugly */
+
+	/**
+	 * Formates the value and give it the right type
+	 * @param string    $rValue
+	 * @param string    $rType
+	 * @param reference $Value
+	 * @param reference $Type
+	 */
+	private function FormatValue($rValue, $rType, $Node, &$Value, &$Type) {
+		preg_match('/[a-zA-Z]+/', $rType, $mType);
+		switch ($mType[0]) {
+			case 'string':
+				$Value = (string)$rValue;
+				if(defined('ACP'))
+					$Type = $this->ParseType($Type, $rType, true);
+				break;
+			case 'int':
+				$Value = (int)$rValue;
+				if(defined('ACP'))
+					$Type = $this->ParseType($Type, $rType);
+				break;
+			case 'float':
+				$Value = (float)$rValue;
+				if(defined('ACP'))
+					$Type = $this->ParseType($Type, $rType);
+				break;
+			case 'bool':
+				if(in_array($rValue, array('true', '1')))
+					$Value = true;
+				else if(in_array($rValue, array('false', '0')))
+					$Value = false;
+				else
+					throw new SystemException('The config node "'.$Node.'" is in the wrong format. Bool expected, the value is: "'.$Value.'"');
+			default:
+				throw new SystemException('The type ('.self::$Config[$Node]['Type'].') of the node "'.$Node.'" is not a valid type.');
+		}
+	}
+
+	/**
+	 * Parse length or range
+	 * @param reference $Type
+	 * @param string    $rType
+	 */
+	private function ParseType(&$Type, $rType, $OnlyLength = false) {
+		if(!$OnlyLength && preg_match('/(\-{0,1}[0-9]+(\.[0-9]+){0,1}){0,1}\|(\-{0,1}[0-9]+(\.[0-9]+){0,1}){0,1}/', $rType, $Range)) {
+			// Range
+			$Range = $Range[0];
+			$aR = explode('|', $Range);
+			if(isset($aR[0]) && !empty($aR[0]) && isset($aR[1]) && !empty($aR[1])) {
+				// Range
+				if($aR[0] < $aR[1]) {
+					$Type = array('Range' => array('Min' => (int)$aR[0], 'Max' => (int)$aR[1]));
+				}
+			} else if(isset($aR[0]) && !empty($aR[0])){ // Min
+				$Type = array('Range' => array('Min' => (int)$aR[0]));
+			} else { // Max
+				$Type = array('Range' => array('Max' => (int)$aR[1]));
+			}
+		} else if(preg_match('/[0-9]+/', $rType, $Length)) {
+			// Length
+			$Type = array('Length' => $Length);
+		}
 	}
 }
 ?>
