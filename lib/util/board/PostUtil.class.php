c4524f2baea04b6623d65678f821b77b595d3494
ozzyfant <ozzy2345de@gmail.com>
2012-12-03 14:30:19 +0100

    Method for creating posts

--- lib/util/board/PostUtil.class.php
+++ lib/util/board/PostUtil.class.php
@@ -6,6 +6,78 @@
  */
 class PostUtil {
 
-}
+	/**
+	 * Creates a new post and returns the post object of the new post.
+	 * @param int $ThreadId
+	 * @param int $UserId
+	 * @param string $Message
+	 * @param string $IpAddress
+	 * @param bool $Smileys
+	 * @param bool $Html
+	 * @param bool $SilexCode
+	 * @return bool|Post Returns the Post object of the new post on success.
+	 */
+	public static function Create($ThreadId, $UserId, $Message, $IpAddress, $Smileys, $Html, $SilexCode) {
 
-?>
+		$Row = [
+			'ThreadID'     => $ThreadId,
+			'UserID'       => $UserId,
+			'Message'      => $Message,
+			'Time'         => time(),
+			'LastEdit'     => '0',
+			'EditorID'     => '0',
+			'PollID'       => '0',
+			'IPAddress'    => $IpAddress,
+			'Disabled'     => '0',
+			'Deleted'      => '0',
+			'DeleteReason' => '',
+			'DeleteTime'   => '0',
+			'Smileys'      => ($Smileys ? '1' : '0'),
+			'HTML'         => ($Html ? '1' : '0'),
+			'SilexCode'    => ($SilexCode ? '1' : '0'),
+		];
+
+		// Write the row in the DB
+		$Query = 'INSERT INTO `post` ';
+
+		$Columns = '(';
+		$Values = 'VALUES(';
+
+		// Arguments for the statementv
+		$Args = [];
+
+		$i = 1;
+		foreach($Row as $Column => $Value) {
+
+			$Columns .= '`'.$Column.'`';
+			$Values  .= ':'.$Column;
+
+			if($i++ != count($Row)) {
+				$Columns .= ', ';
+				$Values  .= ', ';
+			}
+
+			// Arguments for the statement
+			$Args[':'.$Column] = $Value;
+
+		}
+
+		$Columns .= ') ';
+		$Values  .= ')';
+
+		$Query .= $Columns.$Values;
+
+		$Stmt = SBB::DB()->prepare($Query);
+
+		if($Stmt->execute($Args) === false)
+			return false;
+
+		// Get ID
+		$Row['ID'] = SBB::DB()->lastInsertId();
+
+		return new Post(Post::GIVEN_ROW, (object) $Row);
+
+	}
+
+
+}
