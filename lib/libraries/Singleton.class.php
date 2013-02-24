29fb80e3dbe2f7fff03e7238fad7c36f52e5789a
XxidroxX <nicoales@live.it>
2013-02-24 19:04:04 +0100

    Test

--- lib/libraries/Singleton.class.php
+++ lib/libraries/Singleton.class.php
@@ -5,14 +5,25 @@
  * @license     GPL version 3 <http://www.gnu.org/licenses/gpl-3.0.html>
  */
 
-class Singleton {
-	public static function GetInstance() {
-		if(!static::$Instance)
-			static::$Instance = new static;
-		return static::$Instance;
-	}
-
-	private function __clone() {} 
-
-	protected function __construct() {}
+class Singleton{
+ 
+    // Store class instance
+    private static $Instance;
+     
+    // A private constructor; prevents direct creation of object
+    protected function __construct(){}
+ 
+    // Get Singleton Instance
+    public static function GetInstance(){
+        if (!isset(self::$Instance)) {
+            $c = __CLASS__; //get class name
+            self::$Instance = new $c; //create new instance
+        }
+        return self::$Instance;
+    }
+     
+    // Prevent users to clone the instance
+    private function __clone(){
+        trigger_error('Clone is not allowed.', E_USER_ERROR);
+    }
 }
